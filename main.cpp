840eca804ddbb1b05af14ab7aed57c0c14b51144
zx1239856 <zx1239856@gmail.com>
2018-05-01 16:54:55 +0800

    Add insert,select,update and delete interface

--- main.cpp
+++ main.cpp
@@ -13,110 +13,24 @@ config globalConf("./config.conf");
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    std::vector<QString> fields{"id","typename","privilege"};
-    std::vector<QVariantList> vals{{1,2,3},{"Admin","Stock","Test"},{false,false,false}};
-    sql::insert in("lib_settings.stafftype",fields,vals);
-    in.exec();
- /*
-    auto insert = [&]() {
-        auto query(control.query()); // 这里的query在解引用（ -> 或者 * ）后返回的是 QSqlQuery ，直接用就可以了，不需要单独打开数据库或者其他的初始化
-
-        query->prepare("INSERT INTO Persons (id, City) VALUES (?, 123)"); // 模拟插入操作
-
-        query->addBindValue(rand() % 1280);
-
-        if (!query->exec())
-        {
-            qDebug() << "Error" << __LINE__;
-        }
-
-        query->clear();
-    };
-    auto delete_ = [&]() {
-        auto query(control.query());
-
-        query->prepare("delete from Persons where id = ?");
-
-        query->addBindValue(rand() % 1280);
-
-        if (!query->exec())
-        {
-            qDebug() << "Error" << __LINE__;
-        }
-        query->clear();
-    };
-    auto update = [&]() {
-        auto query(control.query());
-
-        query->prepare("update Persons set City = 1111 where id = ?");
-
-        query->addBindValue(rand() % 1280);
-        query->addBindValue(rand() % 1280);
-
-        if (!query->exec())
-        {
-            qDebug() << "Error" << __LINE__;
-        }
-
-        query->clear();
-    };
-    auto select = [&]() {
-        auto query(control.query());
-
-        query->prepare("select * from Persons where id = ?");
-
-        query->addBindValue(rand() % 1280);
-
-        if (!query->exec())
-        {
-            qDebug() << "Error" << __LINE__;
-        }
-
-        query->clear();
-
-        query->prepare("select * from Test2 where data1 = ?");
-
-        query->addBindValue(rand() % 1280);
-
-        if (!query->exec())
-        {
-            qDebug() << "Error" << __LINE__;
-        }
-    };*/
-
+    QMap<QString,QVariantList> map;
+    map.insert(map.end(),"typename",{"user"});
+    map.insert(map.end(),"privilege",{true});
+    map.insert(map.end(),"id",{1});
+    sql::insert up("lib_settings.stafftype",map);
+    up.exec();
+    sql::select sel("lib_settings.stafftype");
+    sel.exec();
+    while(sel.next())
+      {
+        qWarning()<<sel.value(0).toString();
+        qWarning()<<sel.value(1).toString();
+        qWarning()<<sel.value(2).toString()<<endl;
+      }
     /*QThreadPool::globalInstance()->setMaxThreadCount(10);
 
-    for (int now = 0; now < 1; now++) // 开启3个线程测试
-    {
-        QtConcurrent::run([&]() {
-            while (true)
-            {
-                switch (rand() % 20)
-                {
-                case 0:
-                {
-                    insert();
-                    break;
-                }
-                case 1:
-                {
-                    delete_();
-                    break;
-                }
-                case 2:
-                {
-                    update();
-                    break;
-                }
-                default:
-                {
-                    select();
-                    break;
-                }
-                }
-            }
-            QThread::msleep(10);
-        });
+    for Loop indicates how many threads
+    and put the function into QtConcurrent::run() to run
     }*/
 
     return a.exec();
--- utils/dbwrapper/db-operation.cpp
+++ utils/dbwrapper/db-operation.cpp
@@ -12,48 +12,131 @@ dbWrapper::Control* basicSQL::mainDBControl=dbConn::getInstance()->getControl();
 // singleton init end
 
 /*
- * SQL constructors and excutors
+ * SQL constructors and executors
  */
+
+basicSQL::basicSQL():
+  query(mainDBControl->query()){}
+
+basicSQL::basicSQL(const QString &s):
+  query(mainDBControl->query()),sql(s){}
+
+bool basicSQL::exec()
+{
+  return query->exec(sql);
+}
+
+QSqlError basicSQL::lastError()
+{
+  return query->lastError();
+}
+
 insert::insert(const QString &_tableName,
-       const std::vector<QString> &_fields,
-       const std::vector<QVariantList> &_vals):
+               const QMap<QString,QVariantList> &_content):
   basicSQL(),
-  tablename(_tableName),fields(_fields),vals(_vals)
+  tablename(_tableName),content(_content)
 {
   // construct SQL sentence
   sql="INSERT INTO "+_tableName +" (";  // INSERT INTO tablename (a,b,c) VALUES (?, ? ,?)
-  for(auto it=fields.begin();it!=fields.end();++it)
+  for(auto it=content.keys().begin();it!=content.keys().end();++it)
     {
       sql += *it;
-      if((it+1)!=fields.end())sql+=",";
+      if((it+1)!=content.keys().end())sql+=",";
     }
   sql+=") VALUES (";
-  for(auto it=fields.begin();it!=fields.end();++it)
+  for(auto it=content.keys().begin();it!=content.keys().end();++it)
     {
-      sql += ":"+(*it);
-      if((it+1)!=fields.end())sql+=",";
+      sql.append(':');
+      sql += *it;
+      if((it+1)!=content.keys().end())sql+=",";
     }
   sql+=")";
 }
 
-void insert::exec()
+bool insert::exec()
 {
-  auto query(mainDBControl->query());
   query->prepare(sql);
-  for(uint i=0;i<fields.size();++i)
+  for(int i=0;i<content.keys().size();++i)
     {
-      query->bindValue(":"+fields[i],vals[i]);
+      query->bindValue(":"+content.keys()[i],content.values()[i]);
     }
-  query->execBatch();
+  return query->execBatch();
 }
 
-update::update(const QString &_table,const QString &_filter):
-  tablename(_table),filter(_filter)
-{}
-
-void update::exec()
+update::update(const QString& _tablename,
+               QMap<QString,QVariant>& _content,
+               const QString& _filter):
+  basicSQL(),
+  tablename(_tablename),content(_content),filter(_filter)
 {
+  sql="UPDATE "+tablename+" SET ";
+  for(auto it=content.begin();it!=content.end();++it)
+    {
+      sql.append(it.key()+"=:");
+      sql.append(it.key());
+      if((it+1)!=content.end())sql.append(",");
+    }
+  sql+=" WHERE ";
+  sql+=filter;
+}
 
+bool update::exec()
+{
+  query->prepare(sql);
+  for(auto it=content.begin();it!=content.end();++it)
+    {
+      query->bindValue(":"+it.key(),it.value());
+    }
+  return query->exec();
+}
+
+del::del(const QString &_tablename,const QString &_filter):
+  basicSQL(),
+  tablename(_tablename),filter(_filter)
+{
+  sql="DELETE FROM "+tablename +" WHERE ";
+  sql+=filter;
+}
+
+bool del::exec()
+{
+  return query->exec(sql);
+}
+
+select::select(const QString& _tablename,const QString _filter):
+  basicSQL(),
+  tablename(_tablename),filter(_filter)
+{
+  sql="SELECT * FROM "+tablename+" WHERE "+filter;
+}
+
+select::select(const QVector<QString> &_what,const QString& _tablename,const QString _filter):
+  basicSQL(),
+  what(_what),tablename(_tablename),filter(_filter)
+{
+  sql="SELECT ";
+  for(auto it=what.begin();it!=what.end();++it)
+    {
+      sql+=(*it);
+      if((it+1)!=what.end())sql+=",";
+    }
+  sql+=" FROM" + tablename+" WHERE ";
+  sql+=filter;
+}
+
+bool select::exec()
+{
+  return query->exec(sql);
+}
+
+bool select::next()
+{
+  return query->next();
+}
+
+QVariant select::value(int i)
+{
+  return query->value(i);
 }
 
 // dbConn class
