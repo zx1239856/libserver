a0ff4ac2abaa1f954870300cd117de02ff6e167c
zx1239856 <zx1239856@gmail.com>
2018-05-21 14:40:31 +0800

    Add PDF watermark function

--- src/utils/file/pdfhandler.cpp
+++ src/utils/file/pdfhandler.cpp
@@ -1,7 +1,9 @@
 #include "pdfhandler.h"
+#include <QPainter>
+#include <QtMath>
 
 pdfHandler::pdfHandler(const QString& Path):
-  document(nullptr),filePath(Path),numPages(0),pageSize(0,0)
+  document(nullptr),filePath(Path),waterMark(nullptr),rotation(0),opacity(0.5),line(1),full(true),numPages(0),pageSize(0,0)
 {
     document = Poppler::Document::load(filePath);
     if (!document || document->isLocked()) {
@@ -37,6 +39,10 @@ QImage pdfHandler::getPdfImage(int pNum, pdf::resolution res)const
       default: dpi=72;
       }
     image = pdfPage->renderToImage(dpi, dpi, -1, -1, -1, -1); // 72 dpi
+    if(waterMark)
+      {
+         addWaterMark(image,*waterMark,rotation,opacity,line,full);
+      }
     if (image.isNull()) {
         delete pdfPage;
         throw("Image conversion failed.");
@@ -53,10 +59,80 @@ QSize pdfHandler::getPageSize()const
 {
     return pageSize;
 }
+
 void pdfHandler::getPdfInfo() {
     numPages = document->numPages();
     Poppler::Page* pdfPage = document->page(0);
     if(!pdfPage)throw("Error parsing pages.");
     pageSize = pdfPage->pageSize();
     delete pdfPage;
+}
+
+bool pdfHandler::addWaterMark(QImage &src,const QImage &watermark,qreal rotation, qreal opacity, int line, bool full)const
+{
+  if(!watermark.hasAlphaChannel())
+    {
+      qWarning() << "Watermark does not have alpha channel. Please use watermark with alpha channel.";
+      return false;
+    }
+  if(line<0)
+    {
+      qWarning() << "Invalid watermark number.";
+      return false;
+    }
+  if(opacity<0||opacity>1)
+    {
+      qWarning() << "Invalid opacity number.";
+      return false;
+    }
+  rotation=-rotation;
+  QPainter painter(&src);
+  int ylines = line;
+  qreal yoffset = static_cast<qreal>(src.height())/(ylines);
+  // resize the watermark to fit with the bg
+  qreal ratio=1.0*std::min(src.height(),src.width())/std::max(watermark.height(),watermark.width());
+  QImage water = watermark.scaledToHeight(watermark.height()*ratio/ylines);
+  water = water.scaledToWidth(watermark.width()*ratio/ylines);
+  // set opacity
+  painter.setOpacity(opacity);
+  int xlines = src.width()*1.0*ylines/src.height();
+  qreal xoffset = src.width()/(xlines+1);
+  // start to draw watermark
+  for(int i=0;i<ylines;++i)
+    {
+      painter.translate(0,yoffset*(i+1)-yoffset/2);
+      if(ylines==1)
+        {
+          painter.translate(src.width()/2,0);
+          painter.rotate(rotation);
+          painter.drawImage(-0.5*water.width(),-0.5*water.height(),water);
+        }
+      else
+        {
+          for(int j=0;j<=xlines+1;++j)
+            {
+              bool condition;
+              if(!full)condition=!(i%2)^(j%2);
+              else condition=true;
+              if(condition)
+                {
+                  painter.rotate(rotation);
+                  painter.drawImage(-0.5*water.width(),-0.5*water.height(),water);
+                  painter.rotate(-rotation);
+                }
+              painter.translate(xoffset,0);
+            }
+        }
+      painter.resetTransform();
+    }
+  return true;
+}
+
+void pdfHandler::setWatermark(const QImage &water,qreal rot, qreal op, int l,bool f)
+{
+  waterMark=&water;
+  rotation=rot;
+  opacity=op;
+  line=l;
+  full=f;
 }
--- src/utils/file/pdfhandler.h
+++ src/utils/file/pdfhandler.h
@@ -18,12 +18,21 @@ public:
     QImage getPdfImage(int pageNum, pdf::resolution res = pdf::normal)const;
     int getNumPages()const;
     QSize getPageSize()const;
+    void setWatermark(const QImage &watermark,qreal rot=0, qreal op=0.5, int l=1, bool full=true);
 private:
     Poppler::Document* document;
     QString filePath;
+    // watermark parameter
+    const QImage *waterMark;
+    qreal rotation;
+    qreal opacity;
+    int line;
+    bool full;
+    // pdf document parameter
     int numPages;
     QSize pageSize;
     void getPdfInfo();
+    bool addWaterMark(QImage &src, const QImage &watermark,qreal rotation, qreal opacity, int line, bool full)const;
 };
 
 #endif // PDFHANDLER_H
